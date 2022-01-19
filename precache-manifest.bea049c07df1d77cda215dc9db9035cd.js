eae77fe7dd49b03d1f6e999b7119b063f18ef9a5
AugustineEbiem <greataugustine@gmail.com>
2022-01-19 17:43:18 +0100

    Updates

---
+++ precache-manifest.bea049c07df1d77cda215dc9db9035cd.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e8c7d657c933bd0681ca3a5c58fa16e4",
+    "url": "/portfolio-site/index.html"
+  },
+  {
+    "revision": "b10dc0a67abde34c9839",
+    "url": "/portfolio-site/static/css/main.dbe363c7.chunk.css"
+  },
+  {
+    "revision": "50c5744e3374ded30222",
+    "url": "/portfolio-site/static/js/2.09bde94a.chunk.js"
+  },
+  {
+    "revision": "2eb8a6bca50ef7e319d263b58c8b033c",
+    "url": "/portfolio-site/static/js/2.09bde94a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b10dc0a67abde34c9839",
+    "url": "/portfolio-site/static/js/main.3ac918bf.chunk.js"
+  },
+  {
+    "revision": "fbd4ef5810ec8ab5f3b0",
+    "url": "/portfolio-site/static/js/runtime-main.1da0f12a.js"
+  },
+  {
+    "revision": "f9a44e1ccb9f44cd4de1da8fe7f6f4d8",
+    "url": "/portfolio-site/static/media/arrow-left.f9a44e1c.svg"
+  },
+  {
+    "revision": "dc372a07777e87891aaac087cfbe60ad",
+    "url": "/portfolio-site/static/media/arrow-right.dc372a07.svg"
+  },
+  {
+    "revision": "e84c205576ce51cc6a90653c49df11f0",
+    "url": "/portfolio-site/static/media/curology-min.e84c2055.png"
+  },
+  {
+    "revision": "379bad944ff3f73142989e87c5b24773",
+    "url": "/portfolio-site/static/media/lumin-min.379bad94.png"
+  },
+  {
+    "revision": "942c81fd7c87df7da8c098906a29d3fe",
+    "url": "/portfolio-site/static/media/up-arrow-circle.942c81fd.svg"
+  },
+  {
+    "revision": "5f9e30771ee4dbbad1689af16cb49028",
+    "url": "/portfolio-site/static/media/yourspace-min.5f9e3077.png"
+  }
+]);
